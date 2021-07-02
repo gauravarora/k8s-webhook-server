6d1748eba574609e77ebda2ee20258a1939caa10
Gaurav Arora <gaurav.arora@africa.airtel.com>
2021-07-02 22:49:04 +0530

    send a false status always

--- src/main/java/africa/airtel/devops/k8svalidator/controller/AdmissionController.java
+++ src/main/java/africa/airtel/devops/k8svalidator/controller/AdmissionController.java
@@ -1,7 +1,5 @@
 package africa.airtel.devops.k8svalidator.controller;
 
-<<<<<<< HEAD
-=======
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
