11376c9d4432d3f093e1f3ef5bea9233c6ed3b7d
guneetginnigarg <guneet.garg@africa.airtel.com>
2021-07-02 22:38:22 +0530

    Add dockerizing to the project

---
+++ docker/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+java -version
+java -jar k8s-validator-0.0.1-SNAPSHOT.jar
--- src/main/java/africa/airtel/devops/k8svalidator/controller/AdmissionController.java
+++ src/main/java/africa/airtel/devops/k8svalidator/controller/AdmissionController.java
@@ -2,7 +2,7 @@ package africa.airtel.devops.k8svalidator.controller;
 
 import io.fabric8.kubernetes.api.model.admission.v1.AdmissionReview;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AdmissionController {
 
-  @GetMapping("/validate")
-  public AdmissionReview validateAdmissionReview(@RequestBody AdmissionReview requestBody) {
+  @PostMapping("/validate")
+  public AdmissionReview validateAdmissionReview(@RequestBody String requestBody) {
     log.info("Request Body: {}", requestBody);
-    return requestBody;
+    return null;
   }
 }
